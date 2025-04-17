e67cc223ceeb8ee4b794d94468029a7e790c2a20
Ahzyuan <hzy_dev@yeah.net>
2025-04-17 18:50:47 +0800

    fix fetch code in pb workflow

--- torchmeter/__init__.py
+++ torchmeter/__init__.py
@@ -14,7 +14,7 @@ Torchmeter: An `all-in-one` tool for `Pytorch` model analysis, measuring:
 Project: https://github.com/TorchMeter/torchmeter
 """
 
-__version__ = "0.1.3-beta"
+__version__ = "0.1.0"
 
 from torchmeter.core import Meter
 from torchmeter.config import get_config
